fd1602889d666b8f6b63ca3b11c2dab6c64dc668
Sardor <sardor.tozhiev1994@gmail.com>
2026-08-18 15:57:53 +0300

    formated with ruff

--- books_service/models.py
+++ books_service/models.py
@@ -5,7 +5,6 @@ from drf_library_practice import settings
 
 
 class Book(models.Model):
-
     class CoverChoices(models.TextChoices):
         HARD = "HARD"
         SOFT = "SOFT"
@@ -25,6 +24,7 @@ class Book(models.Model):
     class Meta:
         ordering = ["title", "inventory"]
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, unique=True)
@@ -37,15 +37,9 @@ class Borrowing(models.Model):
     borrowing_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
-    book = models.ForeignKey(
-        Book,
-        on_delete=models.CASCADE,
-        related_name='borrowings'
-    )
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='borrowings'
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
     )
 
     def __str__(self):
@@ -56,7 +50,6 @@ class Borrowing(models.Model):
 
 
 class Payment(models.Model):
-
     class StatusChoices(models.TextChoices):
         PENDING = "PENDING"
         PAID = "PAID"
@@ -75,9 +68,7 @@ class Payment(models.Model):
         choices=TypeChoices.choices,
     )
     borrowing = models.ForeignKey(
-        Borrowing,
-        on_delete=models.PROTECT,
-        related_name='payments'
+        Borrowing, on_delete=models.PROTECT, related_name="payments"
     )
     session_url = models.URLField(max_length=500)
     session_id = models.CharField(max_length=255)
--- books_service/serializers.py
+++ books_service/serializers.py
@@ -7,20 +7,12 @@ User = get_user_model()
 
 
 class BookSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Book
-        fields = (
-            "title",
-            "author",
-            "cover",
-            "inventory",
-            "daily_fee"
-        )
+        fields = ("title", "author", "cover", "inventory", "daily_fee")
 
 
 class PaymentSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Payment
         fields = (

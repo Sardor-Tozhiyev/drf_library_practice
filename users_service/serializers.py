779cf204ebf5a70dae0766378ce6994a10f9fde5
Sardor <sardor.tozhiev1994@gmail.com>
2026-08-18 15:10:35 +0300

    create User and create user serializers

---
+++ users_service/serializers.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+        )
+        read_only_fields = ("id",)
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8,
+    )
+    class Meta:
+        model = User
+        fields = (
+            "id",
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+        )
+        read_only_fields = ("id")

b225793cb29dea44fb784cebed8d3d7c98f8a512
Sardor <sardor.tozhiev1994@gmail.com>
2026-08-18 15:25:37 +0300

    create endpoints for users

---
+++ users_service/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users_service.views import CreateUserView, ManageUserView, UserListView, UserDetailView
+
+app_name = "users"
+
+urlpatterns = [
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", ManageUserView.as_view(), name="me"),
+    path("", UserListView.as_view(), name="user_list"),
+    path("<int:pk>/", UserDetailView.as_view(), name="user_detail"),
+]
+

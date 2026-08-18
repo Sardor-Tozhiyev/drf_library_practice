ed3fbd97fbb7d9506ed11febda91b47619ea653a
Sardor <sardor.tozhiev1994@gmail.com>
2026-08-18 14:20:30 +0300

    create User views for create and manage

--- books_service/views.py
+++ books_service/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from books_service.serializers import (
+    UserCreateSerializer,
+    UserSerializer,
+)
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserCreateSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user

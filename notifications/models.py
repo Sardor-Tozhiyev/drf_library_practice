a6f5e1d539457f59f78145d062211c0d22fa0ee8
Sardor <sardor.tozhiev1994@gmail.com>
2026-08-19 12:03:55 +0300

    add notifications app and NotificationLog model

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class NotificationLog(models.Model):
+    pass
+
+
+class NotificationType(models.TextChoices):
+    NEW_BORROWING = "New borrowing"
+    OVERDUE_BORROWING = "Overdue borrowing"
+    SUCCESSFUL_PAYMENT = "Successful payment"
+
+
+notification_type = models.CharField(
+    max_length=30,
+    choices=NotificationType.choices,
+)
+message_text = models.TextField()
+is_sent = models.BooleanField(default=False)
+error_message = models.TextField(blank=True, null=True)
+created_at = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self) -> str:
+    return f"{self.notification_type} ({'sent' if self.is_sent else 'failed'})"
